4bd1ea32cf7eb6282d7ab335c620bf02db8341a6
awesomeosep <99228798+awesomeosep@users.noreply.github.com>
2025-09-07 20:48:58 -0400

    listen page

--- spleeter_flutter_app/lib/listen.dart
+++ spleeter_flutter_app/lib/listen.dart
@@ -15,6 +15,17 @@ class _ListenState extends State<Listen> {
   bool currentlyPlaying = false;
 
   @override
+  void initState() {
+    super.initState();
+
+    player.setReleaseMode(ReleaseMode.stop);
+
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await player.setSource(DeviceFileSource(file.path));
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     file = ModalRoute.of(context)!.settings.arguments as File;
 
@@ -23,7 +34,31 @@ class _ListenState extends State<Listen> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text("Now playing:"), SizedBox(height: 16), Text(file.path.split('/').last)],
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [Icon(Icons.audio_file), SizedBox(width: 8), Text(file.path.split('/').last)],
+            ),
+            SizedBox(height: 32),
+            IconButton.filledTonal(
+              onPressed: () {
+                if (currentlyPlaying) {
+                  player.pause();
+                } else {
+                  if (player.state == PlayerState.paused) {
+                    player.resume();
+                  } else {
+                    player.play(DeviceFileSource(file.path));
+                  }
+                }
+                setState(() {
+                  currentlyPlaying = !currentlyPlaying;
+                });
+              },
+              icon: currentlyPlaying ? Icon(Icons.pause) : Icon(Icons.play_arrow),
+            ),
+          ],
         ),
       ),
     );
